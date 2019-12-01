ca6de4fe8f7c92ee0da267df334754f21fbe64c1
ZhengsenF <42655384+ZhengsenF@users.noreply.github.com>
2019-11-30 21:07:21 -0500

    started decl

--- src/compiler.java
+++ src/compiler.java
@@ -9,24 +9,37 @@ public class compiler {
     public static void main(String[] args) throws IOException {
         File inputFile = new File(args[0]);
         DataOutputStream output = new DataOutputStream(new FileOutputStream(args[1]));
-        Map<String, Integer> symbolTable;
+        Map<String, Integer> symbolTable; // key: symbol; value: offset on the stack
         byteCode bc = new byteCode(output);
         try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
             String line;
             while ((line = br.readLine()) != null) {
                 if(line.matches("decl[A-Za-z 0-9]*")) {
-                    Pattern pattern = Pattern.compile("([0-9]+)");
+                    Pattern pattern = Pattern.compile("decl ([a-zA-Z]+) ([a-z]*)");
                     Matcher matcher = pattern.matcher(line);
+                    if(!matcher.find()) {
+                        System.out.println("decl Error!");
+                    }
+                    String symbol = matcher.group(1);
+                    String dataType = matcher.group(2);
+//                    symbolTable.put(symbol, )
+                    if(dataType.equals("short")) {
+                        bc.pushs((short) 0);
+                    }
+                    if(dataType.equals("int")) {
+                        bc.pushi(0);
+                    }
+                    if(dataType.equals("float")) {
+                        bc.pushf(0);
+                    }
                 }
-                if(line.matches("subr[A-Za-z 0-9]*")) {
+                if(line.matches("subr[A-Za-z 0-9]*")) { //unfinished
                     Pattern pattern = Pattern.compile("([0-9]+)");
                     Matcher matcher = pattern.matcher(line);
                     if(!matcher.find()) {
                         System.out.println("subr Error!");;
                     }
-                    bc.pushs((short) 1);
-
-                    break;
+                    continue;
                 }
             }
         }
